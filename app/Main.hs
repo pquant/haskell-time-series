76c86045002383e12f8c1e883bb1bca9ccb2a417
pquant <pquant@hotmail.fr>
2016-04-14 17:46:00 +0100

    Changed underlying Series type to Data.Vector + cleaned up typeclass and instances contexts

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,5 @@
 {-# OPTIONS_GHC -fwarn-unused-imports #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
 -- import Lib
@@ -8,14 +9,53 @@ import qualified Data.Series as S
 main :: IO ()
 -- main=someFunc
 main = do
+----------------------------------------------------------
+-- Play with timeDiffs on DateTimes
+----------------------------------------------------------
+    let dt1 = H.DateTime (H.Date 2016 H.January 1) (H.TimeOfDay 19 40 1 0)
+        dt2 = H.DateTime (H.Date 2016 H.January 2) (H.TimeOfDay 19 40 0 0)
+        diff = H.timeDiff dt2 dt1
+        diffP = H.timeDiffP dt2 dt1
+    print "diff:" >> print diff
+    print "diffP:" >> print diffP
+    print "-----------------------------------"
+----------------------------------------------------------
+-- Play with time-series creation
+----------------------------------------------------------
     let ns = [1 ..10]
         tod = H.TimeOfDay 19 40 0 0
         ds  = map (H.Date 2016 H.January) ns
         dts = map (`H.DateTime` tod) ds
         ldts  = map H.localTimeFromGlobal dts
-        --ts0 = S.create ds ns
+        ts0 = S.create ds ns
         ts1 = S.create dts ns
         ts2 = S.create ldts ns
-    --print $ S.values ts0
+    print ts0
     print ts1
     print ts2
+    print "-----------------------------------"
+----------------------------------------------------------
+-- More Play with timeDiffs
+----------------------------------------------------------
+    -- Time difference tests
+    let todPlus2Secs = H.DateTime (H.Date 2016 H.January 1) $ tod {H.todSec = H.Seconds 2}
+        todPlus1Sec = H.DateTime (H.Date 2016 H.January 1) $ tod {H.todSec = H.Seconds 1}
+        todPlus1NanoSec = H.DateTime (H.Date 2016 H.January 1) $ tod {H.todNSec = H.NanoSeconds 1}
+        todPlusLoadsNanoSecs = H.DateTime (H.Date 2016 H.January 1) $ tod {H.todNSec = H.NanoSeconds 10^9+1}
+        todRef = H.DateTime (H.Date 2016 H.January 1) $ tod
+    print (H.timeDiff todPlus2Secs todRef)
+    print (H.timeDiff todPlus1Sec todRef)
+    print (H.timeDiff todPlus1NanoSec  todRef)
+    print (H.timeDiff todPlusLoadsNanoSecs todRef)
+
+    print (H.timeDiffP todPlus2Secs todRef)
+    print (H.timeDiffP todPlus1Sec todRef)
+    print (H.timeDiffP todPlus1NanoSec  todRef)
+    print (H.timeDiffP todPlusLoadsNanoSecs todRef)
+
+
+    print ((\(x,y) -> x + H.toSeconds y)$ H.timeDiffP todPlus2Secs todRef)
+    print ((\(x,y) -> x + H.toSeconds y)$ H.timeDiffP todPlus1Sec todRef)
+    print ((\(x,y) -> x + H.toSeconds y)$ H.timeDiffP todPlus1NanoSec  todRef)
+    print ((\(x,y) -> x + H.toSeconds y)$ H.timeDiffP todPlusLoadsNanoSecs todRef)
+    print "-----------------------------------"
--- src/Data/Series.hs
+++ src/Data/Series.hs
@@ -1,40 +1,56 @@
+{-#LANGUAGE ScopedTypeVariables#-}
 module Data.Series
 (
 -- * Type
-Series, -- export Type only. The official constructor is `create`
+Series, -- export Type only. The official (safe) constructor is `create`
 
 -- * Construction/Conversion
 create, createUnsafe,
 toTuples,
 
 -- * Accessors
-values
+values, timeStamps
 ) where
 
 import qualified Data.Hourglass as H
-import qualified Data.Map       as M
+import qualified Data.Vector    as V
 import qualified Data.List      as L
-
+import Data.Function (on)
 --------------------------------------------------------------
 -- TimeStamp class
 --------------------------------------------------------------
 -- | TimeStamp class to identify which Hourglass time type is suitable for time-series
 --
 -- (Not all of them are, plus some functionalities are time-type specific)
-class (Ord t, Eq t) => TimeStamp t where
+--
+-- A TimeStamp has to be a Timeable because HourGlass.timeDiff is called in the safe contructor function 'create'
+class H.Timeable t  => TimeStamp t where
     printTimeStamp :: H.TimeFormat frmt => frmt -> t -> String
 
-instance (H.Time t, Ord t, Eq t) => TimeStamp (H.LocalTime t) where
+instance TimeStamp t => TimeStamp (H.LocalTime t) where
     printTimeStamp = H.localTimePrint
 
 instance TimeStamp H.DateTime where
     printTimeStamp = H.timePrint
 
+instance TimeStamp H.Date where
+    printTimeStamp = H.timePrint
+
+--------------------------------------------------------------
+-- Extra Timeable instances
+--
+-- Confirm with Vincent and see if that can be integrated into Hourglass
+--------------------------------------------------------------
+instance H.Timeable t => H.Timeable (H.LocalTime t) where
+    timeGetElapsedP = H.timeGetElapsedP . H.localTimeUnwrap
+
 --------------------------------------------------------------
 -- Series Type
 --------------------------------------------------------------
 -- | Series data type
-data Series t a = Series (M.Map t a) deriving(Eq)
+--
+-- Does not yet allow sub-seccond difference between Timestamps if constructed using the safe `create` function
+data Series t a = Series (V.Vector (t,a)) deriving(Eq)
 
 instance (TimeStamp t, Show a) => Show (Series t a) where
     show ts =
@@ -48,32 +64,55 @@ instance (TimeStamp t, Show a) => Show (Series t a) where
 --------------------------------------------------------------
 -- Construction/Conversion
 --------------------------------------------------------------
+-- | Safe construction which guarantees underlying Vector (t,a) to be ordered by t
+--
+-- Uses Data.List.sort (mergeSort) on t - should be O(nlogn) best/worst/average
 create
     :: TimeStamp t -- (Ord t, Eq t)
     => [t]
     -> [a]
     -> Series t a
-create xs ys = Series $ M.fromList $ zip xs ys
+create ts xs = Series $ V.fromList $ L.sortBy (compareFun `on` fst) $ zip ts xs
+    where
+        compareFun :: TimeStamp t => t -> t -> Ordering
+        compareFun ti tj
+            | abs diffSecs < 1  = error errMsg
+            | otherwise         = if signum diffSecs == 1 then GT else LT
+            where
+                diffSecs= secs + H.toSeconds nanoSecs where (secs, nanoSecs) = H.timeDiffP ti tj
+                errMsg =    "\nSafe time-series constructions currently requires TimeStamps not to be less than 1 second apart, " ++
+                            "otherwise TimeStamps are considered equal, which cannot happen in time-series.\n" ++
+                            "The following time-stamps do not satisfy this criterion:\n" ++
+                            (printTimeStamp H.ISO8601_DateAndTime ti) ++ " and " ++ (printTimeStamp H.ISO8601_DateAndTime tj) ++
+                            " have a time difference (in seconds) of " ++ show diffSecs
 
+
+-- | Assumes TimeStamps are in increasing order and distinguishable (i.e. no less than 1 second apart)
 createUnsafe
     :: TimeStamp t
     => [t]
     -> [a]
     -> Series t a
-createUnsafe xs ys = Series $ M.fromAscList $ zip xs ys
+createUnsafe ts xs = Series $ V.fromList $ zip ts xs
 
 -- | Returns a list of 2-tuples from a Series a
 toTuples
     :: TimeStamp t
     => Series t a
     -> [(t, a)]
-toTuples (Series m) = M.toAscList m
+toTuples (Series v) = V.toList v
 
---------------------------------------------------------------
--- Accessors
---------------------------------------------------------------
+----------------------------------------------------------------
+---- Accessors
+----------------------------------------------------------------
 -- | Returns the values of Series a
 values
     :: Series t a
     -> [a]
-values (Series m)= M.elems m
+values (Series v) = snd . unzip . V.toList $ v
+
+-- | Returns the timeStamps of Series a
+timeStamps
+    :: Series t a
+    -> [t]
+timeStamps (Series v) = fst . unzip . V.toList $ v
